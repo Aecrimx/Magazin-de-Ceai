a4ae550aa383cfac3640ff01aec212c3f11b3971
Aecrimx <mircea.alexandru.popa@gmail.com>
2026-03-23 14:33:54 +0200

    adaugat documentul updatat

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express= require("express");
 const path= require("path");
+const fs = require("fs");
+const sass = require("sass");
 
 app= express();
 app.set("view engine", "ejs")
@@ -22,12 +24,20 @@ console.log("Cale fisier", __filename);
 
 // });
 
+obGlobal={
+    obErori:null,
+    obImagini:null,
+    folderScss: path.join(__dirname, "resurse/scss"),
+    folderCss: path.join(__dirname, "resurse/css"),
+    folderBackup: path.join(__dirname, "backup"),
+}
+
 
 app.get("/cale/:a/:b", function(req, res){
     res.send(parseInt(req.params.a) + parseInt(req.params.b));
     console.log("cerere parsata");
 }); // parametrii in cerere
-app.get("/", function(req, res){
+app.get(["/", "/index", "/home"], function(req, res){ //acel vector ne lasa sa avem "alias-uri" pt main page
     // res.sendFile(path.join(__dirname, "index.html")); 
     res.render("pagini/index");   
 })
@@ -35,6 +45,8 @@ app.get("/", function(req, res){
 // o metoda mai smart este:
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
 // asa nu trb sa scriem pt fiecare fisier css un sendFile
+//mereu concatanam cu __Dirname si nu cu current working directory pt ca dirname e relativ la entry point, cwd e locatia de unde se apeleaza
+
 
 app.get("/:a/:b", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"));
@@ -43,9 +55,84 @@ app.get("/:a/:b", function(req, res){
 
 
 
+
+function initErori(){
+    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
+    let erori=obGlobal.obErori=JSON.parse(continut)
+    let err_default=erori.eroare_default
+    err_default.imagine=path.join(erori.cale_baza, err_default.imagine)
+    for (let eroare of erori.info_erori){
+        eroare.imagine=path.join(erori.cale_baza, eroare.imagine)
+    }
+
+}
+initErori()
+
+app.get("/eroare", function(req, res){
+    res.render("pagini/eroare", {
+        imagine: obGlobal.ObErori.eroare_default.imagine,
+        titlu: obGlobal.obErori.eroare_default.titlu,
+        text: obGlobal.obErori.eroare_default.text,
+    });
+}) // testam o eroare.
+
+
+function compileazaScss(caleScss, caleCss){
+    if(!caleCss){
+
+        let numeFisExt=path.basename(caleScss); // "folder1/folder2/a.scss" -> "a.scss"
+        let numeFis=numeFisExt.split(".")[0]   /// "a.scss"  -> ["a","scss"]
+        caleCss=numeFis+".css"; // output: a.css
+    }
+    
+    if (!path.isAbsolute(caleScss))
+        caleScss=path.join(obGlobal.folderScss,caleScss )
+    if (!path.isAbsolute(caleCss))
+        caleCss=path.join(obGlobal.folderCss,caleCss )
+    
+    let caleBackup=path.join(obGlobal.folderBackup, "resurse/css");
+    if (!fs.existsSync(caleBackup)) {
+        fs.mkdirSync(caleBackup,{recursive:true})
+    }
+    
+    // la acest punct avem cai absolute in caleScss si  caleCss
+
+    let numeFisCss=path.basename(caleCss);
+    if (fs.existsSync(caleCss)){
+        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, "resurse/css",numeFisCss ))// +(new Date()).getTime()
+    }
+    rez=sass.compile(caleScss, {"sourceMap":true});
+    fs.writeFileSync(caleCss,rez.css)
+    
+}
+
+
+//la pornirea serverului
+vFisiere=fs.readdirSync(obGlobal.folderScss);
+for( let numeFis of vFisiere ){
+    if (path.extname(numeFis)==".scss"){
+        compileazaScss(numeFis);
+    }
+}
+
+fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
+    if (eveniment=="change" || eveniment=="rename"){
+        let caleCompleta=path.join(obGlobal.folderScss, numeFis);
+        if (fs.existsSync(caleCompleta)){
+            compileazaScss(caleCompleta);
+        }
+    }
+})
+
+function afisareEroare(res, identificator, titlu, text, imagine){
+    //TO DO cautam eroarea dupa identificator
+    //daca sunt setate titlu, text, imagine, le folosim, 
+    //altfel folosim cele din fisierul json pentru eroarea gasita
+    //daca nu o gasim, afisam eroarea default
+}
+
+
 // nodemon . pt a da run
 
 app.listen(8080);
 console.log("Serverul a pornit!");
-
-
