c4641482181aac5268a176aaf6726d8b581d7e7a
Aecrimx <mircea.alexandru.popa@gmail.com>
2026-03-27 23:38:06 +0200

    portat la ejs

--- index.js
+++ index.js
@@ -1,199 +1,183 @@
-const express= require("express");
-const path= require("path");
+const express = require("express");
+const path = require("path");
 const fs = require("fs");
 const sass = require("sass");
 
-app= express();
-app.set("view engine", "ejs")
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 console.log("Folder index.js", __dirname);
 console.log("Folder curent (de lucru)", process.cwd());
 console.log("Cale fisier", __filename);
 
-//pt taskul cu foldere
-// let vect_foldere = ["temp", "logs", "backup", "fisiere_uploadate"];
-// for(let folder of vect_foldere){
-//     let caleFolder=path.join(__dirname, folder);
-//     if(!fs.existsSync(caleFolder)){
-//         fs.mkdir(caleFolder);
-//     }
-// }
-
-
-// incepem sa definim http requests
-// app.get("/cale", function(req, res){
-//     res.send("Salut, ai ajuns pe <b style='color = red;'>server!</b>");
-//     console.log("Am primit o cerere GET pe /cale");
-// });
-
-
-// app.get("/cale2", function(req, res){
-//     res.write("ceva\n");
-//     res.write("altceva");
-//     res.end();
-
-// });
-
-obGlobal={
-    obErori:null,
-    obImagini:null,
+const obGlobal = {
+    obErori: null,
     folderScss: path.join(__dirname, "resurse/scss"),
     folderCss: path.join(__dirname, "resurse/css"),
-    folderBackup: path.join(__dirname, "backup"),
-}
+    folderBackup: path.join(__dirname, "backup")
+};
 
-
-app.get("/cale/:a/:b", function(req, res){
-    res.send(parseInt(req.params.a) + parseInt(req.params.b));
-    console.log("cerere parsata");
-});// parametrii in cerere
-
-
-app.get(["/", "/index", "/home"], function(req, res){ //acel vector ne lasa sa avem "alias-uri" pt main page
-    // res.sendFile(path.join(__dirname, "index.html")); 
-    res.render("pagini/index", {
-        ip: req.ip // si in ejs putem folosi <%- locals.ip> ca sa il vedem
-        //parametrul transmis in render se numeste locals
-    });   
-})
-//e gresit sa returnam pt fiecare get un alt fisier.
-// o metoda mai smart este:
 app.use("/resurse", express.static(path.join(__dirname, "resurse")));
-// asa nu trb sa scriem pt fiecare fisier css un sendFile
-//mereu concatanam cu __Dirname si nu cu current working directory pt ca dirname e relativ la entry point, cwd e locatia de unde se apeleaza
 
-//pt task-ul cu favicon.ico
-app.get("/favicon.ico", function(req, res){
-    res.sendFIle(path.joib(__dirname, "resurse/imagini/favicon/favicon.ico"))
+app.get("/favicon.ico", function (req, res) {
+    res.sendFile(path.join(__dirname, "resurse/imagini/favicon/favicon.ico"));
 });
 
+function initErori() {
+    const continut = fs
+        .readFileSync(path.join(__dirname, "resurse/json/erori.json"))
+        .toString("utf-8");
 
-app.get("/:a/:b", function(req, res){
-    res.sendFile(path.join(__dirname, "index.html"));
-    console.log(parseInt(req.params.a) + parseInt(req.params.b));
-}) // asta e gresit pt ca 
+    const erori = JSON.parse(continut);
+    const caleBaza = erori.cale_baza;
 
+    erori.eroare_default.imagine = path.join(caleBaza, erori.eroare_default.imagine);
 
-
-
-function initErori(){
-    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
-    let erori=obGlobal.obErori=JSON.parse(continut)
-    let err_default=erori.eroare_default
-    err_default.imagine=path.join(erori.cale_baza, err_default.imagine)
-    for (let eroare of erori.info_erori){
-        eroare.imagine=path.join(erori.cale_baza, eroare.imagine)
+    for (const eroare of erori.info_erori) {
+        eroare.imagine = path.join(caleBaza, eroare.imagine);
     }
 
+    obGlobal.obErori = erori;
 }
-initErori()
 
-app.get("/eroare", function(req, res){
-    res.render("pagini/eroare", {
-        imagine: obGlobal.ObErori.eroare_default.imagine,
-        titlu: obGlobal.obErori.eroare_default.titlu,
-        text: obGlobal.obErori.eroare_default.text,
-    });
-}) // testam o eroare.
-
-//cum facem mai multe pagini fara sa facme cpy paste app.get? see line 141 inainte de app.listen
-
-
-
-
-
-function compileazaScss(caleScss, caleCss){
-    if(!caleCss){
-
-        let numeFisExt=path.basename(caleScss); // "folder1/folder2/a.scss" -> "a.scss"
-        let numeFis=numeFisExt.split(".")[0]   /// "a.scss"  -> ["a","scss"]
-        caleCss=numeFis+".css"; // output: a.css
+function getEroareById(identificator) {
+    if (!obGlobal.obErori) {
+        return {
+            titlu: "Eroare",
+            text: "A survenit o eroare.",
+            imagine: "/resurse/imagini/erori/interzis.png"
+        };
     }
-    
-    if (!path.isAbsolute(caleScss))
-        caleScss=path.join(obGlobal.folderScss,caleScss )
-    if (!path.isAbsolute(caleCss))
-        caleCss=path.join(obGlobal.folderCss,caleCss )
-    
-    let caleBackup=path.join(obGlobal.folderBackup, "resurse/css");
+
+    if (typeof identificator === "undefined") {
+        return obGlobal.obErori.eroare_default;
+    }
+
+    const eroareGasita = obGlobal.obErori.info_erori.find(
+        (err) => err.identificator === identificator && err.status === true
+    );
+
+    return eroareGasita || obGlobal.obErori.eroare_default;
+}
+
+function afisareEroare(res, identificator, titlu, text, imagine) {
+    const eroare = getEroareById(identificator);
+
+    const dateRandare = {
+        titlu: titlu || eroare.titlu,
+        text: text || eroare.text,
+        imagine: imagine || eroare.imagine
+    };
+
+    if (Number.isInteger(identificator) && identificator >= 400) {
+        res.status(identificator);
+    }
+
+    if (identificator === 404) {
+        res.render("pagini/404", dateRandare);
+        return;
+    }
+
+    res.render("pagini/eroare", dateRandare);
+}
+
+function compileazaScss(caleScss, caleCss) {
+    if (!caleCss) {
+        const numeFisExt = path.basename(caleScss);
+        const numeFis = numeFisExt.split(".")[0];
+        caleCss = `${numeFis}.css`;
+    }
+
+    if (!path.isAbsolute(caleScss)) {
+        caleScss = path.join(obGlobal.folderScss, caleScss);
+    }
+
+    if (!path.isAbsolute(caleCss)) {
+        caleCss = path.join(obGlobal.folderCss, caleCss);
+    }
+
+    const caleBackup = path.join(obGlobal.folderBackup, "resurse/css");
     if (!fs.existsSync(caleBackup)) {
-        fs.mkdirSync(caleBackup,{recursive:true})
+        fs.mkdirSync(caleBackup, { recursive: true });
     }
-    
-    // la acest punct avem cai absolute in caleScss si  caleCss
 
-    let numeFisCss=path.basename(caleCss);
-    if (fs.existsSync(caleCss)){
-        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, "resurse/css",numeFisCss ))// +(new Date()).getTime()
+    const numeFisCss = path.basename(caleCss);
+    if (fs.existsSync(caleCss)) {
+        fs.copyFileSync(caleCss, path.join(caleBackup, numeFisCss));
     }
-    rez=sass.compile(caleScss, {"sourceMap":true});
-    fs.writeFileSync(caleCss,rez.css)
-    
+
+    const rez = sass.compile(caleScss, { sourceMap: true });
+    fs.writeFileSync(caleCss, rez.css);
 }
 
-
-//la pornirea serverului
-vFisiere=fs.readdirSync(obGlobal.folderScss);
-for( let numeFis of vFisiere ){
-    if (path.extname(numeFis)==".scss"){
-        compileazaScss(numeFis);
-    }
-}
-
-fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
-    if (eveniment=="change" || eveniment=="rename"){
-        let caleCompleta=path.join(obGlobal.folderScss, numeFis);
-        if (fs.existsSync(caleCompleta)){
-            compileazaScss(caleCompleta);
+function initScss() {
+    const vFisiere = fs.readdirSync(obGlobal.folderScss);
+    for (const numeFis of vFisiere) {
+        if (path.extname(numeFis) === ".scss") {
+            compileazaScss(numeFis);
         }
     }
-})
 
-function afisareEroare(res, identificator, titlu, text, imagine){
-    //TO DO cautam eroarea dupa identificator
-    //daca sunt setate titlu, text, imagine, le folosim, 
-    //altfel folosim cele din fisierul json pentru eroarea gasita
-    //daca nu o gasim, afisam eroarea default
+    fs.watch(obGlobal.folderScss, function (eveniment, numeFis) {
+        if (!numeFis) {
+            return;
+        }
+
+        if (eveniment === "change" || eveniment === "rename") {
+            const caleCompleta = path.join(obGlobal.folderScss, numeFis);
+            if (fs.existsSync(caleCompleta) && path.extname(numeFis) === ".scss") {
+                compileazaScss(caleCompleta);
+            }
+        }
+    });
 }
 
+initErori();
+initScss();
 
-// nodemon . pt a da run
+app.get(["/", "/index", "/home"], function (req, res) {
+    res.render("pagini/index", {
+        ip: req.ip
+    });
+});
 
-app.get("/*pagina", function(req,res){ //intern acm va cauta pagina respectiva
-    console.log("Cale pagina", req.url);
-    //daca poate renda .render(), il tirmite in rezultatRendare
-    //daca nu poate, apeleaza functia callback
-    if(req.url.startsWith("/resurse") && path.extname(req.url)==""){
-        //e director, nu fisier.
-        afisareEroare(res, 403);
+app.get("/eroare", function (req, res) {
+    afisareEroare(res);
+});
+
+app.get("/:pagina", function (req, res) {
+    const pagina = req.params.pagina;
+
+    if (pagina === "favicon.ico") {
         return;
     }
-    if(path.extname(req.url) == ".ejs"){
-        afisareEroare(res, 400); //badrequest
+
+    if (pagina.endsWith(".ejs")) {
+        afisareEroare(res, 400);
         return;
     }
-    try{
-            res.render("pagini"+req.url, function(err, rezRendare){
-        if(err){
-            if(err.message.includes("Failed to lookup view")){
-                afisareEroare(res, 404)
+
+    res.render(`pagini/${pagina}`, function (err, rezultatRandare) {
+        if (err) {
+            if (err.message.includes("Failed to lookup view")) {
+                afisareEroare(res, 404);
                 return;
             }
+
             afisareEroare(res);
             return;
         }
-        res.send(rezRendare);
-        console.log("Randare", rezRandare); // verificam e un html duh
+
+        res.send(rezultatRandare);
     });
-    }catch(err){
-        if(err.message.includes("Cannot find module")){
-            afisareEroare(res, 404);
-            return;
-        }
-        afisareEroare(res);
-        return;
-    }
 });
 
-app.listen(8080);
-console.log("Serverul a pornit!");
+app.use(function (req, res) {
+    afisareEroare(res, 404);
+});
+
+app.listen(8080, function () {
+    console.log("Serverul a pornit pe portul 8080!");
+});
