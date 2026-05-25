0232339378063930aa75a19fa462ce224daa1971
Aecrimx <mircea.alexandru.popa@gmail.com>
2026-05-25 14:58:22 +0300

    banner animat, javascript si cookies adaugat

--- resurse/js/cookies.js
+++ resurse/js/cookies.js
@@ -1,10 +1,7 @@
-
-
-//setCookie("a",10, 1000)
 function setCookie(nume, val, timpExpirare){//timpExpirare in milisecunde
     d=new Date();
     d.setTime(d.getTime()+timpExpirare)
-    document.cookie=`${nume}=${val}; expires=${d.toUTCString()}`;
+    document.cookie=`${nume}=${val}; expires=${d.toUTCString()}; path=/`; // path=/ ca sa fie persistent global
 }
 
 function getCookie(nume){
@@ -18,17 +15,29 @@ function getCookie(nume){
 
 function deleteCookie(nume){
     console.log(`${nume}; expires=${(new Date()).toUTCString()}`)
-    document.cookie=`${nume}=0; expires=${(new Date()).toUTCString()}`;
+    document.cookie=`${nume}=0; expires=${(new Date()).toUTCString()}; path=/`;
 }
 
 
 window.addEventListener("load", function(){
+    const bannerElement = document.getElementById("banner-proiect-scolar");
+    const okButton = document.getElementById("ok_cookies");
+    
+    console.log("Banner element:", bannerElement);
+    console.log("OK button:", okButton);
+    
     if (getCookie("acceptat_banner")){
-        document.getElementById("banner").style.display="none";
+        bannerElement.hidden = true;
+    } else {
+        bannerElement.hidden = false;
     }
 
-    this.document.getElementById("ok_cookies").onclick=function(){
-        setCookie("acceptat_banner",true,60000);
-        document.getElementById("banner").style.display="none"
+    if (okButton) {
+        okButton.onclick = function(){
+            console.log("cookie acceptat");
+            setCookie("acceptat_banner", true, 86400000); // Cookie expiră după 1 zi
+            // setCookie("acceptat_banner", true, 10000); // 10 secunde pentru testare comportamentului
+            bannerElement.hidden = true;
+        }
     }
 })
